59243060e9c4c9d414f70930a2c0439c3aeeabc2
Nikita Mittal <nikitamittal2423@gmail.com>
2026-09-04 11:03:10 +0530

    fix(utils): update BACKEND_IP to use new google server address

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// const BACKEND_IP = "35.240.222.126"; // new google server
-const BACKEND_IP = "localhost:3000";
+const BACKEND_IP = "35.240.222.126"; // new google server
+// const BACKEND_IP = "localhost:3000";
 // const BACKEND_IP = "192.168.31.234"; // local dev
 
 export const NEXT_PUBLIC_API_URL = `http://${BACKEND_IP}`;
